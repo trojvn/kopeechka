1f70025cdfb2120eec5a2368c5b2fdeb140f168c
trojvn <mrx.trojan@gmail.com>
2024-04-17 12:07:50 +0300

    initial commit

---
+++ kopeechka/mailwrapper.py
@@ -0,0 +1,39 @@
+import contextlib
+from typing import Literal
+
+import httpx
+
+SITES = Literal["telegram.org", "tiktok.com"]
+MAIL_TYPES = Literal["gmail.com", "rambler.ru", "outlook.com"]
+
+
+class KopeechkaMailWrapper:
+    def __init__(self, token: str):
+        self.__base_url, self.__token = "https://api.kopeechka.store", token
+
+    def get_mail(
+        self,
+        site: SITES,
+        mail_type: MAIL_TYPES,
+        regex: str = "",
+    ) -> str:
+        url = f"{self.__base_url}/mailbox-get-email"
+        params = {
+            "token": self.__token,
+            "site": site,
+            "mail_type": mail_type,
+            "regex": regex,
+        }
+        with contextlib.suppress(Exception):
+            json_data = httpx.get(url, params=params).json()
+            return f"{json_data['mail']}|{json_data['id']}"
+        return "ERROR"
+
+    # noinspection PyShadowingBuiltins
+    def get_message(self, id: str) -> str:
+        url = f"{self.__base_url}/mailbox-get-message"
+        params = {"token": self.__token, "id": id}
+        with contextlib.suppress(Exception):
+            json_data = httpx.get(url, params=params).json()
+            return str(json_data["value"])
+        return "WAIT_LINK"
